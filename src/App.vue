08a5391c845123c80b680162d395b35e6fedd2fd
Karama-karama <aissaoui.karama@gmail.com>
2022-04-14 04:40:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Antd from 'ant-design-vue';
+import Home from './components/Home.vue';
+import 'ant-design-vue/dist/antd.css';
+</script>
+
+<template>
+  <!-- Image goes here i you want to  -->
+  <Home />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from 'vue';
+import { MDCDataTable } from '@material/data-table';
+
+defineProps({
+  msg: String,
+  alert: String,
+});
+//Declaring const here
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    fixed: true,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+  },
+];
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description:
+      'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description:
+      'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description:
+      'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+  },
+];
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h5>{{ alert }}</h5>
+  <a-space align="center" style="margin-bottom: 16px">
+    CheckStrictly:
+    <a-switch v-model:checked="rowSelection.checkStrictly"></a-switch>
+  </a-space>
+  <a-table
+    :columns="columns"
+    :data-source="data"
+    :row-selection="rowSelection"
+  />
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup() {
+    return {
+      data,
+      columns,
+    };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
